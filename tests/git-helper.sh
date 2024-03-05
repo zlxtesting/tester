a5c4d794f165021ee3c1db581b23b1c1e8853074
servicedev4397863487@gmail.com <hannay.almohanna@netspi.com>
2024-03-05 18:26:06 +0000

    test4

--- tests/git-helper.sh
+++ tests/git-helper.sh
@@ -22,8 +22,8 @@ function rebase_atop_of_the_latest_target_branch() {
 }
 
 function main() {
-	echo "$KATA_GITHUB_ACTIONS_PR_SIZE_TOKEN"
-	
+	echo "$GITHUB_TOKEN"
+
     action="${1:-}"
 
     add_kata_bot_info
